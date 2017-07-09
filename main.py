90c8b6cf53eff12aeb42c0487c9a6f678243c323
Pawel Brola <brola71@gmail.com>
2017-07-09 17:41:52 +0100

    Add simple return

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask
+
+import note
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/note/<note_id>")
+def get_note(note_id):
+    return note.read_note(note_id)
